2bc03b5c32104d581947594c7af4eed4fc23b81c
vfa-bientv <103976711+vfa-bientv@users.noreply.github.com>
2023-02-27 00:38:48 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<Router>
+		<Router basename="/portfolio">
 			<App />
 		</Router>
 	</React.StrictMode>
