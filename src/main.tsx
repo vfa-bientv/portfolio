6603794c239ac7a8d6f5e182c09abb61e0c61d71
vfa-bientv <103976711+vfa-bientv@users.noreply.github.com>
2023-02-27 07:30:01 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<HashRouter basename="/portfolio">
+		<Router basename="/portfolio">
 			<App />
-		</HashRouter>
+		</Router>
 	</React.StrictMode>
 );
